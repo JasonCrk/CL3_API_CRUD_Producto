51464de8425020d54ed7d8f2386feefdb0eb3606
JasonCrk <emerzonkolki@gmail.com>
2024-05-09 00:51:30 -0500

    feat(ProductRequest.java): Add validations

--- src/main/java/com/cibertec/cl3/models/dto/ProductRequest.java
+++ src/main/java/com/cibertec/cl3/models/dto/ProductRequest.java
@@ -1,11 +1,18 @@
 package com.cibertec.cl3.models.dto;
 
-import java.time.LocalDate;
+import jakarta.validation.constraints.*;
 
 public record ProductRequest(
+        @NotBlank(message = "The description is required")
+        @Size(max = 255, message = "The description only allows a maximum of 255 characters")
         String description,
-        LocalDate registrationDate,
+
+        @NotNull(message = "The price is required")
+        @DecimalMin(value = "00.1", message = "The price cannot be negative")
         double price,
+
+        @NotNull(message = "The stock is required")
+        @Positive(message = "The stock cannot be negative")
         int stock
 ) {
 }
