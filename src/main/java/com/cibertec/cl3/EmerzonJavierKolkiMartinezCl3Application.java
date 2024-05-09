bc42882e3e22db071fc1b43eda7566647c308887
JasonCrk <emerzonkolki@gmail.com>
2024-05-08 23:26:07 -0500

    feat: Initial commit

---
+++ src/main/java/com/cibertec/cl3/EmerzonJavierKolkiMartinezCl3Application.java
@@ -0,0 +1,13 @@
+package com.cibertec.cl3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmerzonJavierKolkiMartinezCl3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmerzonJavierKolkiMartinezCl3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/cl3/EmerzonJavierKolkiMartinezCl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.cl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmerzonJavierKolkiMartinezCl3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
