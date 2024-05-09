89d9a3355f8421fbdf7358ea37bb0f714e97480b
JasonCrk <emerzonkolki@gmail.com>
2024-05-09 00:17:30 -0500

    feat: Add product mapper

---
+++ src/main/java/com/cibertec/cl3/models/mappers/ProductMapper.java
@@ -0,0 +1,30 @@
+package com.cibertec.cl3.models.mappers;
+
+import com.cibertec.cl3.models.Product;
+import com.cibertec.cl3.models.dto.ProductRequest;
+import com.cibertec.cl3.models.dto.ProductResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public Product toModel(ProductRequest request) {
+        return Product.builder()
+                .description(request.description())
+                .price(request.price())
+                .registrationDate(request.registrationDate())
+                .stock(request.stock())
+                .build();
+    }
+
+    public ProductResponse toResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getDescription(),
+                product.getRegistrationDate(),
+                product.getPrice(),
+                product.getStock()
+        );
+    }
+}
